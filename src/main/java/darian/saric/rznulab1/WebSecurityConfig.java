dc239cb2843a46e9644c10ba71bd6fdbbec734c6
dariansaric <darian.saric@fer.hr>
2019-11-30 19:02:32 +0100

    Ostaje napraviti home page

---
+++ src/main/java/darian/saric/rznulab1/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package darian.saric.rznulab1;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/nfl/**").hasRole("USER")
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+        ;
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(users.username("Roger Godell").password("ihatetombrady").roles("USER").build());
+
+        return manager;
+    }
+}
